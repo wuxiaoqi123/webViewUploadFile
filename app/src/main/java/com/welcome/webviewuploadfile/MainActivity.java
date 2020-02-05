3f716f277af6f75ea805f750ce459345e2c5f262
wuxiaoqi <1321972760@qq.com>
2020-02-05 09:32:13 +0800

    commit

---
+++ app/src/main/java/com/welcome/webviewuploadfile/MainActivity.java
@@ -0,0 +1,124 @@
+package com.welcome.webviewuploadfile;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ValueCallback<Uri> uploadFile;
+    private ValueCallback<Uri[]> uploadFiles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebChromeClient(new WebChromeClient() {
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                Log.i("test", "openFileChooser 1");
+                uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsgs) {
+                Log.i("test", "openFileChooser 2");
+                uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android  > 4.1.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.i("test", "openFileChooser 3");
+                uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android  >= 5.0
+            public boolean onShowFileChooser(WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+                Log.i("test", "openFileChooser 4:" + filePathCallback.toString());
+                uploadFiles = filePathCallback;
+                openFileChooseProcess();
+                return true;
+            }
+
+        });
+
+        webView.loadUrl("file:///android_asset/fileChooser.html");
+    }
+
+    private void openFileChooseProcess() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("*/*");
+        startActivityForResult(Intent.createChooser(i, "test"), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 0:
+                    if (uploadFile != null) {
+                        Uri result = data == null || resultCode != RESULT_OK ? null
+                                : data.getData();
+                        uploadFile.onReceiveValue(result);
+                        uploadFile = null;
+                    }
+                    if (uploadFiles != null) {
+                        Uri result = data == null || resultCode != RESULT_OK ? null
+                                : data.getData();
+                        uploadFiles.onReceiveValue(new Uri[]{result});
+                        uploadFiles = null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            if (uploadFile != null) {
+                uploadFile.onReceiveValue(null);
+                uploadFile = null;
+            }
+            if (uploadFiles != null) {
+                uploadFiles.onReceiveValue(null);
+                uploadFiles = null;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.onPause();
+    }
+
+    /**
+     * 确保注销配置能够被释放
+     */
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+}
